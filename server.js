2e5fc2b0f53ad7dfe1bfc11098b4da127e8273a6
Willson97 <vermawillson@gmail.com>
2017-09-02 13:04:22 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,9 +84,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:articleName',function(req,res){
-    //articleName=article-one;
+    //articleName=articleone;
     //articles[articleName]=={} content object for article one
-    var arctileName=req.params.articleName;
+    var arctileName= req.params.articleName;
      res.send(createTemplate(articles[articleName]));
 });
 
