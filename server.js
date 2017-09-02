abd1144cbe602db68dd2277e1ba6c8f9355f15e3
Willson97 <vermawillson@gmail.com>
2017-09-02 12:23:06 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/articleone',function(req,res){
-     res.send(createTemplate(articleOne));
+     res.send(createTemplate(articleone));
 });
 
 app.get('/articletwo',function(req,res){
